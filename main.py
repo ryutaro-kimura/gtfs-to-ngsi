bb286818adcc2e79b6bc58b038b543a9220039fc
ryutaro-kimura <sasuker2264@gmail.com>
2023-01-23 17:19:49 +0900

    post stop.txt to fiware

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # makeOurCity
 
+import numpy as np
+from fastapi import FastAPI
+import pandas as pd
 import json
 import os
 
@@ -23,11 +26,6 @@ def get_auth() -> AuthBase:
     return auth
 
 
-import pandas as pd
-from fastapi import FastAPI
-import json
-import numpy as np
-
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.integer):
@@ -38,49 +36,57 @@ class NpEncoder(json.JSONEncoder):
             return obj.tolist()
         return super(NpEncoder, self).default(obj)
 
+
 app = FastAPI()
+
+
 @app.get("/stops")
 async def stop():
-  df = pd.read_csv('./data/stops.txt', header=0)
-  # print(df.loc[0])
-  test = {}
-  try:
-    for gtfs_stop in df.loc:
-      # print (gtfs_stop)
-      ngsi_stop = {
-        'id': "urn:ngsi-ld:GtfsStop:{}".format(gtfs_stop.stop_id),
-        "type": "GtfsStop",
-        "code": gtfs_stop.stop_code,
-        "name": gtfs_stop.name,
-        "location": {
-          "type": gtfs_stop.location_type,
-          "coordinates": [gtfs_stop.stop_lat, gtfs_stop.stop_lon]
-        },
-        "operateBy": ["urn:ngsi-ld:GtfsStop:{}".format(gtfs_stop.stop_id)]
-      }
-      ngsi_stop_json = json.dumps(ngsi_stop, cls=NpEncoder)
+    df = pd.read_csv('./data/stops.txt', header=0)
+    # print(df.loc[0])
+    test = {}
+    try:
+        for gtfs_stop in df.loc:
+            # print (gtfs_stop)
+            ngsi_stop = {
+                'id': "urn:ngsi-ld:GtfsStop:{}".format(gtfs_stop.stop_id),
+                "type": "GtfsStop",
+                "code": "",
+                # "code": gtfs_stop.stop_code,
+                "name": gtfs_stop.name,
+                "location": {
+                    "type": gtfs_stop.location_type,
+                    "coordinates": [gtfs_stop.stop_lat, gtfs_stop.stop_lon]
+                },
+                "operateBy": ["urn:ngsi-ld:GtfsStop:{}".format(gtfs_stop.stop_id)]
+            }
+            ngsi_stop_json = json.dumps(ngsi_stop, cls=NpEncoder)
 
-      # １つだけPOSTしてみる
-      if(ngsi_stop["name"] == 4369):
-        test = ngsi_stop_json
-        print(test)
-        orion_endpoint = os.getenv("ORION_ENDPOINT")
-        auth = get_auth()
-        try:
-          requests.post(orion_endpoint + "/v2/entities", auth=auth, data=test)
-          print("OK-------------")
-        except Exception as e:
-           print('post exception：',e)
-      
-      # print("==",ngsi_stop_json)
+            # １つだけPOSTしてみる
+            if (ngsi_stop["name"] == 4369):
+                test = ngsi_stop_json
+                print("test=====", test)  # ここは出力されてる
+                orion_endpoint = os.getenv("ORION_ENDPOINT")
+                auth = get_auth()
+                try:
+                    req = requests.post(orion_endpoint + "/v2/entities?options=keyValues",
+                                        auth=auth, data=test, headers={'content-type': 'application/json'})
+                    print("req===", req)
+                    print("req.json=====", req.json())
+                    print("OK-------------")
+                except Exception as e:
+                    print('post exception：', e)
 
-  except Exception as e:
-    print("Error===",e)
+            # print("==",ngsi_stop_json)
 
-  # orion_endpoint = os.getenv("ORION_ENDPOINT")
-  # auth = get_auth()
-  # response = requests.get(orion_endpoint + "/v2/entities", auth=auth)
-  response = requests.get(orion_endpoint + "/v2/entities?GtfsStop", auth=auth)
-  inOrion = json.dumps(response.json(), indent=2)
-  
-  return {"name":inOrion}
+    except Exception as e:
+        print("Error===", e)
+
+    # orion_endpoint = os.getenv("ORION_ENDPOINT")
+    # auth = get_auth()
+    # response = requests.get(orion_endpoint + "/v2/entities", auth=auth)
+    response = requests.get(
+        orion_endpoint + "/v2/entities?type=GtfsStop", auth=auth)
+    inOrion = json.dumps(response.json(), indent=2)
+
+    return {"name": inOrion}
